4ef611f2aa80b7fe4c61ea340aafb51ffe3b71bd
Caleb84Loarca <cloarcam@miumg.edu.gt>
2019-10-15 12:55:29 -0600

    VALE INGRESO
    ingreso de vales para compra

--- core/app/view/libros-view.php
+++ core/app/view/libros-view.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-12">
 <div class="btn-group pull-right">
-	<a href="index.php?view=newlibro" class="btn btn-default"><i class='fa fa-book'></i> Nuevo Vendedor</a>
+	<a href="index.php?view=newvales" class="btn btn-default"><i class='fa fa-book'></i> Nuevo Vale</a>
 <div class="btn-group pull-right">
   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
     <i class="fa fa-download"></i> Descargar <span class="caret"></span>
@@ -11,7 +11,7 @@
   </ul>
 </div>
 </div>
-		<h1>Listado de Libros</h1>
+		<h1>Listado de Vales</h1>
 <br>
 		<?php
          
--- core/app/view/newvales-view.php
+++ core/app/view/newvales-view.php
@@ -8,71 +8,38 @@ $autores = AutorData::getAll();
     <img src="image/Palki.png" width="350" height="50">
 	<br>
 
-		<form class="form-horizontal" method="post" id="addlibro" action="index.php?view=addlibro" role="form">
+    <form class="form-horizontal" method="post" id="addlibro" action="index.php?view=addlibro" role="form">
 
 
-  <div class="form-group">
-    <div class="col-md-6">   
-        <label for="text" class="col-lg-2 control-label">Vale Compra #</label>      
-        <select name="autor_id" required class="form-control">
-             <?php 
-                foreach($autores as $autor):?>      
-                <option value="<?php echo $autor->id;
-                    ?>">
-                    <?php echo $autor->id." "?>
-                </option>
-            <?php endforeach;?>
-        </select>   
-    </div>
-  </div>
+            <div class="form-group">
+                <div class="col-md-4">   
+                   <h2> <label for="text" class="col-lg-8 control-label">Vale Compra #</label>   </h2>   
+                    <select name="autor_id" required class="form-control">
+                        <?php 
+                            foreach($autores as $autor):?>      
+                            <option value="<?php echo $autor->id;
+                                ?>">
+                                <?php echo $autor->id." "?>
+                            </option>
+                        <?php endforeach;?>
+                    </select>   
+                </div>
+            </div>
  
   <div class="form-group">
-            <div class="col-md-6">
-            <label for="inputEmail1" class="col-lg-2 control-label">Fecha de Compra*</label>
-            <input type="date" name="sd"  required value="<?php if(isset($_GET["sd"])){ echo $_GET["sd"]; }?>" class="form-control">
-            </div>
+            <div class="col-md-3">
+                     <label for="inputEmail1" class="col-lg-6 control-label">Fecha de Compra*</label>
+                        <input type="date" name="sd"  required value="<?php if(isset($_GET["sd"])){ echo $_GET["sd"]; }?>" class="form-control">
 
-            <div class="col-md-4">
-            <label for="inputEmail1" class="col-lg-2 control-label">Pedido</label>
-            <select name="autor_id" required class="form-control">
-                  <option value="">-- NINGUNO --</option>
-                  <?php foreach($autores as $autor):?>
-                    <option value="<?php echo $autor->id;?>"><?php echo $autor->id;?></option>
-              <?php endforeach;?>
-          </select>
-            </div>
-  </div>
+                    <label for="inputEmail1" class="col-lg-2 control-label">Pedido</label>
+                    <select name="autor_id" required class="form-control">
+                                <option value="">-- NINGUNO --</option>
+                        <?php foreach($autores as $autor):?>
+                            <option value="<?php echo $autor->id;?>"><?php echo $autor->id;?></option>
+                        <?php endforeach;?>
+                    </select>
 
-
-<div class="form-group">
-        <div class="col-md-6">
-        <label for="inputEmail1" class="col-lg-2 control-label">Codigo Proveedor*</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->id;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
-  </div>
-  
-
-
-
-  <div class="form-group">
-        <div class="col-md-6">
-        <label for="inputEmail1" class="col-lg-2 control-label">Proveedor*</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
-  </div>
-  
-  <div class="form-group">
-      <div class="col-md-6">
+         
           <label for="inputEmail1" class="col-lg-2 control-label">Region*</label>       
           <select name="autor_id" required class="form-control">
           <option value="">-- NINGUNO --</option>
@@ -80,7 +47,28 @@ $autores = AutorData::getAll();
           <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
           <?php endforeach;?>
           </select>
-      </div>
+      
+
+        </div>
+
+            <div class="col-md-6">
+                <label for="inputEmail1" class="col-lg-6 control-label">Codigo Proveedor*</label>
+                <select name="autor_id" required class="form-control">
+                <option value="">-- NINGUNO --</option>
+                <?php foreach($autores as $autor):?>
+                <option value="<?php echo $autor->id;?>"><?php echo $autor->id;?></option>
+                <?php endforeach;?>
+                </select>
+
+                <label for="inputEmail1" class="col-lg-2 control-label">Proveedor*</label>
+                <select name="autor_id" required class="form-control">
+                <option value="">-- NINGUNO --</option>
+                <?php foreach($autores as $autor):?>
+                <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
+                <?php endforeach;?>
+                </select>
+
+            </div>
 
       <div class="col-md-6">
           <label for="inputEmail1" class="col-lg-2 control-label">Procedencia*</label>
@@ -92,15 +80,13 @@ $autores = AutorData::getAll();
           <?php endforeach;?>
           </select>
       </div>
-  
-  </div>  
-  
-   
+</div> <!-- fin de encabezado de vale-->
 
+<!-- detalle del vale -->
   <div class="form-group">
       
-      <div class="col-md-3">
-      <label for="inputEmail1" class="col-lg-2 control-label">MEDIDA DE PLANTA</label>
+      <div class="col-md-2">
+      <label for="inputEmail1" class="col-lg-10 control-label">MEDIDA DE PLANTA</label>
     
           <select name="autor_id" required class="form-control">
           <option value="">-- NINGUNO --</option>
@@ -112,63 +98,45 @@ $autores = AutorData::getAll();
 
       
       <div class="col-md-2">
-      <label for="inputEmail1" class="col-lg-2 control-label">INVERNADERO</label>
+          <label for="inputEmail1" class="col-lg-2 control-label">INVERNADERO</label>
           <select name="autor_id" required class="form-control">
           <option value="">-- NINGUNO --</option>
           <?php foreach($autores as $autor):?>
           <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
           <?php endforeach;?>
           </select>
-      </div>
-
-
+        </div>
+          
+            <div class="col-md-2">
+                <label for="inputEmail1" class="col-lg-8 control-label">SECCION/CAMA</label>
+                <input type="text" name="seccion" valor="seccion">
+            </div>   
+            <div class="col-md-2">
+                <label for="inputEmail1" class="col-lg-8 control-label">UNIDADES</label>
+                <input type="text" name="unidades" valor="unidades">
+            </div> 
       <div class="col-md-2">
-      <label for="inputEmail1" class="col-lg-2 control-label">SECCION/CAMA</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
+        <label for="inputEmail1" class="col-lg-10 control-label">PRECIO UNITARIO</label>
+        <input type="text" name="precio" valor="precio">
+      </div> 
 
-      <div class="col-md-2">
-      <label for="inputEmail1" class="col-lg-2 control-label">UNIDADES</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
-
-
-      <div class="col-md-2">
-      <label for="inputEmail1" class="col-lg-2 control-label">PRECIO UNITARIO</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
-
-
-      <div class="col-md-3">
-      <label for="inputEmail1" class="col-lg-2 control-label">VALOR TOTAL</label>
-          <select name="autor_id" required class="form-control">
-          <option value="">-- NINGUNO --</option>
-          <?php foreach($autores as $autor):?>
-          <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
-          <?php endforeach;?>
-          </select>
-      </div>
+            <div class="col-md-2">
+                <label for="inputEmail1" class="col-lg-10 control-label">VALOR TOTAL</label>
+                    <select name="autor_id" required class="form-control">
+                    <option value="">-- NINGUNO --</option>
+                    <?php foreach($autores as $autor):?>
+                    <option value="<?php echo $autor->id;?>"><?php echo $autor->nombre." ".$autor->apellido;?></option>
+                    <?php endforeach;?>
+                    </select>
+            
+            </div>
 
   </div>  
 
-
-  <p class="alert alert-info">* Campos obligatorios</p>
-
+<!-- boton y alerta-->
+    <div>
+        <p class="alert alert-info">* Campos obligatorios</p>
+    </div>
   <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
       <button type="submit" class="btn btn-primary">Guardar Vale</button>
